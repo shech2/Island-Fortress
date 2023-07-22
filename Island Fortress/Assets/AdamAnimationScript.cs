67bffdb8b8765939953081fa50ff7011361ef4ff
May Shech <may745@cs.colman.ac.il>
2023-07-22 16:32:15 +0300

    Added A model from Mixamo

---
+++ Island Fortress/Assets/AdamAnimationScript.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AdamAnimationScript : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+
+    void Awake()
+    {
+        anim = GetComponent<Animator>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            anim.SetFloat("speed", 1);
+        }
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            anim.SetFloat("speed", 0);
+        }
+        // left shift
+        if (Input.GetKeyDown(KeyCode.LeftShift))
+        {
+            anim.SetFloat("speed", 2);
+        }
+    }
+}
