65394b704effaf218d0539677177fbc7f9a9c228
May Shech <may745@cs.colman.ac.il>
2023-07-30 13:14:40 +0300

    Update

--- Island Fortress/Assets/Scripts/PlayerController.cs
+++ Island Fortress/Assets/Scripts/PlayerController.cs
@@ -15,7 +15,6 @@ public class PlayerController : MonoBehaviour
     {
         agent = GetComponent<NavMeshAgent>();
         animator = GetComponent<Animator>();
-
         agent.autoBraking = false;
 
         while (true)
@@ -27,20 +26,9 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
-        GameObject enemy = GameObject.FindGameObjectWithTag("Player");
-        float distanceToPlayer = Vector3.Distance(transform.position, enemy.transform.position);
-
-        // If Player is in sight of Enemy and within a certain distance, chase Enemy
-        if ((IsPlayerInSight() && distanceToPlayer <= 10f)  || (IsPlayerInSight() && distanceToPlayer <= 3f))
+        if (IsPlayerInSight())
         {
             ChaseEnemy();
-        }
-        else
-        {
-            animator.SetBool("Run", false);
-            animator.SetBool("Attack", false);
-            agent.isStopped = false;
-            VisitPoints();
         }
     }
 
@@ -51,53 +39,42 @@ public class PlayerController : MonoBehaviour
         Vector3 directionToPlayer = enemy.transform.position - transform.position;
         float angle = Vector3.Angle(directionToPlayer, transform.forward);
 
-        return angle <= targetRange;
+        if (angle < 45f && directionToPlayer.magnitude < targetRange)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, directionToPlayer, out hit))
+            {
+                if (hit.collider.gameObject.tag == "Player")
+                {
+                    Debug.DrawRay(transform.position, directionToPlayer, Color.green);
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     private void VisitPoints()
     {
-        if (points.Length > 0)
+        if (points.Length == 0)
+            return;
+
+        agent.destination = points[pointsIndex].position;
+        animator.SetBool("Run", true);
+        animator.SetBool("Attack", false);
+
+        if (agent.remainingDistance <= agent.stoppingDistance)
         {
-            if (agent.remainingDistance < 0.5f)
-            {
-                if (pointsIndex >= points.Length)
-                {
-                    pointsIndex = 0;
-                }
-                else
-                {
-                    animator.SetBool("Run", false);
-                    animator.SetBool("Attack", false);
-                    agent.SetDestination(points[pointsIndex].position);
-                    pointsIndex++;
-                }
-            }
+            pointsIndex = (pointsIndex + 1) % points.Length;
         }
     }
 
     private void ChaseEnemy()
     {
         GameObject enemy = GameObject.FindGameObjectWithTag("Player");
-        Vector3 directionToPlayer = enemy.transform.position - transform.position;
-        directionToPlayer.y = 0;
-        float distanceToPlayer = Vector3.Distance(transform.position, enemy.transform.position);
-        transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(directionToPlayer), 0.1f);
-
-        if (distanceToPlayer > 3f && distanceToPlayer <= 10f)
-        {
-            animator.SetBool("Run", true);
-            animator.SetBool("Attack", false);
-            agent.isStopped = false;
-            agent.SetDestination(enemy.transform.position);
-
-        }
-        else if(distanceToPlayer <= 3f)
-        {
-            animator.SetBool("Run", false);
-            animator.SetBool("Attack", true);
-            agent.isStopped = true;
-        }
-
+        agent.destination = enemy.transform.position;
+        animator.SetBool("Run", true);
+        animator.SetBool("Attack", false);
     }
 
 }
