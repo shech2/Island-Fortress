8c161535446de3a27e754b8574c485b75b404ffb
SemionSolonar <shimon22361@cs.colman.ac.il>
2023-08-03 13:22:42 +0300

    Added speech to the main character with the animal
    Basically now the main character at the press of a button says Free and then the animal goes for a walk and come back and then the animal comes back

--- Island Fortress/Assets/Scripts/AgentScript.cs
+++ Island Fortress/Assets/Scripts/AgentScript.cs
@@ -6,6 +6,8 @@ public class AgentScript : MonoBehaviour
 {
     public Transform target;
     public AudioClip sound; // sound that will be played when 'M' is pressed
+    public AudioClip freeMonkey; // sound that will be played when 'N' is pressed
+    public AudioClip ComeBack; // sound that will be played when 'B' is pressed
     UnityEngine.AI.NavMeshAgent agent;
     Animator animator;
     AudioSource audioSource;
@@ -22,6 +24,7 @@ public class AgentScript : MonoBehaviour
 
     void Update()
     {
+
         if (shouldFollow)
         {
             agent.SetDestination(target.position);
@@ -55,7 +58,12 @@ public class AgentScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.M))
         {
+            float distance = Vector3.Distance(agent.transform.position, target.position);
             audioSource.PlayOneShot(sound); // Play the sound
+            if(distance>= 5f){
+                audioSource.PlayOneShot(ComeBack); // Play the sound
+            }
+            
             shouldFollow = !shouldFollow;
             if (shouldFollow)
                 pointsIndex = 0;
@@ -63,6 +71,7 @@ public class AgentScript : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.N) && shouldFollow)
         {
+            audioSource.PlayOneShot(freeMonkey); // Play the sound
             shouldFollow = false;
             pointsIndex = 0; // Reset pointsIndex to 0 each time 'N' is pressed
             VisitPoints();
