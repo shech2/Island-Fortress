2f82c25aebac142b283013f4c7a2f2d2d3355e4f
SemionSolonar <shimon22361@cs.colman.ac.il>
2023-08-20 01:12:24 +0300

    Teleport to the start from the exploding box

--- Island Fortress/Assets/Scripts/BoxExplosion.cs
+++ Island Fortress/Assets/Scripts/BoxExplosion.cs
@@ -1,110 +1,181 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.ParticleSystemJobs;
-
-
-
+using UnityEngine.UI;
+using TMPro;
 public class BoxExplosion : MonoBehaviour
 {
     public GameObject originalObject;
     public GameObject fracturedObject;
-    public GameObject explosin;
+    public GameObject explosion;
     public float explosionMinForce = 5;
     public float explosionMaxForce = 100;
     public float explosionRadius = 10;
     public float scale = 1;
     public ParticleSystem particleSystem;
     public AudioClip audioClip;
-
+    public Text promptText;
+    public GameObject PlayerHealthBar;
     private GameObject fractObj;
-    private bool isExploding = false;  // The new flag
-    private List<Coroutine> shrinkCoroutines = new List<Coroutine>(); // List to store Shrink coroutines
-    AudioSource audioSource;
+    private bool isExploding = false;
+    private List<Coroutine> shrinkCoroutines = new List<Coroutine>();
+    private AudioSource audioSource;
+    private GameObject player;
+    private bool hasExploded = false;
+    private float PlayerHealth = 100f;
+    private Vector3 playerStartingPosition;
 
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
-
+        player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerStartingPosition = player.transform.position;
+        }
+        UpdateHealthBar();
     }
 
     void Update()
     {
-        if (!isExploding && Input.GetKeyDown(KeyCode.T))
+        if (IsPlayerNearby() && PlayerIsFacingBox())
         {
-            particleSystem.Play();
-            audioSource.PlayOneShot(audioClip);
-            Explode();
+            if (!isExploding)
+            {
+                if (promptText != null)
+                {
+                    promptText.text = "press Q to open the box";
+                    promptText.enabled = true;
+                }
+
+                if (Input.GetKeyDown(KeyCode.Q))
+                {
+                    if (!gameObject.activeInHierarchy)
+                    {
+                        Debug.LogWarning("Trying to explode an inactive object: " + gameObject.name);
+                        return;
+                    }
+
+                    promptText.enabled = false;
+                    isExploding = true;
+                    Explode();
+                }
+            }
         }
-        if (Input.GetKeyDown(KeyCode.R))
+        else
         {
-            Reset();
+            if (promptText != null)
+            {
+                promptText.enabled = false;
+            }
+        }
+
+        if (PlayerHealth <= 0)
+        {
+            StartCoroutine(ReturnPlayerToStartPosition()); // This will handle the delay before death
         }
     }
 
+
     void Explode()
     {
-        isExploding = true;  // Set the flag to true at the beginning of the operation
+        isExploding = true;
+        hasExploded = true;
+        particleSystem.Play();
+        audioSource.PlayOneShot(audioClip);
+
+        if (fracturedObject != null)
+        {
+            fractObj = Instantiate(fracturedObject) as GameObject;
+            foreach (Transform t in fractObj.transform)
+            {
+                var rb = t.GetComponent<Rigidbody>();
+                if (rb != null)
+                {
+                    rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionRadius);
+                }
+                Coroutine shrinkCoroutine = StartCoroutine(Shrink(t, 7));
+                shrinkCoroutines.Add(shrinkCoroutine);
+            }
+
+            Destroy(fractObj, 8);
+
+            if (explosion != null)
+            {
+                GameObject expl = Instantiate(explosion) as GameObject;
+                Destroy(expl, 7);
+            }
+
+            PlayerHealth -= 100;
+        }
 
         if (originalObject != null)
         {
             originalObject.SetActive(false);
-            if (fracturedObject != null)
-            {
-                fractObj = Instantiate(fracturedObject) as GameObject;
-                foreach (Transform t in fractObj.transform)
-                {
-                    var rb = t.GetComponent<Rigidbody>();
-                    if (rb != null)
-                    {
-                        rb.AddExplosionForce(Random.Range(explosionMinForce, explosionMaxForce), originalObject.transform.position, explosionRadius);
-                    }
-                    Coroutine shrinkCoroutine = StartCoroutine(Shrink(t, 7));
-                    shrinkCoroutines.Add(shrinkCoroutine);
-                }
-
-                Destroy(fractObj, 8);
-
-
-                if (explosin != null)
-                {
-                    GameObject expl = Instantiate(explosin) as GameObject;
-                    Destroy(expl, 7);
-                }
-            }
         }
     }
-
-    void Reset()
-    {
-        foreach (Coroutine coroutine in shrinkCoroutines) // Stop all the Shrink coroutines
-        {
-            StopCoroutine(coroutine);
-        }
-        shrinkCoroutines.Clear(); // Clear the list for next use
-
-        if (fractObj != null)
-        {
-            Destroy(fractObj);
-        }
-        originalObject.SetActive(true);
-        isExploding = false;  // Reset the flag here as well
-    }
-
 
     IEnumerator Shrink(Transform t, float delay)
     {
         yield return new WaitForSeconds(delay);
-        while (t != null && t.gameObject != null) // Check if the GameObject exists
+        while (t != null && t.gameObject != null)
         {
             Vector3 newScale = t.localScale;
-
             if (newScale.x >= 0)
             {
                 newScale -= new Vector3(scale, scale, scale);
                 t.localScale = newScale;
             }
             yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private bool IsPlayerNearby()
+    {
+        if (player != null)
+        {
+            return Vector3.Distance(transform.position, player.transform.position) < 3f;
+        }
+        return false;
+    }
+
+    private bool PlayerIsFacingBox()
+    {
+        if (player != null)
+        {
+            Vector3 toBox = (transform.position - player.transform.position).normalized;
+            float angleToBox = Vector3.Angle(player.transform.forward, toBox);
+            return angleToBox < 45f;
+        }
+        return false;
+    }
+
+    private void Death()
+    {
+        if (player != null)
+        {
+            player.transform.position = playerStartingPosition;
+            PlayerHealth = 0f;
+            UpdateHealthBar();
+        }
+    }
+
+
+
+    private void UpdateHealthBar()
+    {
+        if (PlayerHealthBar != null)
+        {
+            PlayerHealthBar.GetComponent<TextMeshProUGUI>().text = PlayerHealth.ToString();
+        }
+    }
+
+    IEnumerator ReturnPlayerToStartPosition()
+    {
+        yield return new WaitForSeconds(1.5f);
+        if (player != null)
+        {
+            player.transform.position = playerStartingPosition;
         }
     }
 
